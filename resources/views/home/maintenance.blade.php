6cb5e5c1051b201e2344c4aad5ca71e7deb37d39
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2023-02-21 14:48:33 +0700

    update

--- resources/views/home/maintenance.blade.php
+++ resources/views/home/maintenance.blade.php
@@ -4,7 +4,7 @@
     <div class="container position-relative">
       <div class="row gy-5" data-aos="fade-in">
         <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
-          <h2>Coming Soon :)</br></h2>
+          <h2>Coming Soon :)</h2>
           <p>Mohon maaf atas ketidaknyamanannya. Anda bisa menjelajahi page website ini dalam waktu secepatnya. Mohon kembali lagi di lain waktu. Atau anda bisa kembali lagi ke menu <a href="{{url('/')}}">Home</a>.</p>
         </div>
         <div class="col-lg-6 order-1 order-lg-2">
